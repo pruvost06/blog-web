2d2c45c351c561fbcbd581b3bc160ed8c1874ee3
pruvost06 <rolfpruvost@gmail.com>
2021-03-06 20:06:40 +0100

    like-pagevitrine

--- js/min.js
+++ js/min.js
@@ -1,10 +1,50 @@
 
-$(document).ready(() => {
+//-------------jquery---Voyage en Asie-----------------//
+$(document).ready(function(){
+$(".like").click(function(){
+$(this).css("color","red");
+});
+});
 
-$('div').moussenter(() => {
 
-$('.hover-text').css('color','red');
-})
+//-------------jquery---Le sens croissant -----------------//
+$(document).ready(function(){
+  $(".aime").click(function(){
+  $(this).css("color","red");
+  });
+  });
 
-})
+
+//-------------jquery---Ma (nouvelle) liste -----------------//
+$(document).ready(function(){
+  $(".lick").click(function(){
+  $(this).css("color","red");
+  });
+  });
+  
+  //-------------jquery---Meilleures randonnées -----------------//
+$(document).ready(function(){
+  $(".likin").click(function(){
+  $(this).css("color","red");
+  });
+  });
+
+   //-------------jquery---marchés alimentaires -----------------//
+$(document).ready(function(){
+  $(".liking").click(function(){
+  $(this).css("color","red");
+  });
+  });
+
+     //-------------jquery---Les spots de plongée -----------------//
+$(document).ready(function(){
+  $(".doding").click(function(){
+  $(this).css("color","red");
+  });
+  });
+  
+
+
+
+
 
